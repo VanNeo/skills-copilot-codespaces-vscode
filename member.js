70c2c853087eee3564a9c3a03b14d9467922cf6a
VanNeo <83795306+VanNeo@users.noreply.github.com>
2023-07-27 21:48:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ['JavaScript', 'HTML', 'CSS'];
+    var member = {
+        name: 'Irfan',
+        age: 25,
+        skills: skills
+    };
+    console.log(member.skills[0]);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+}
